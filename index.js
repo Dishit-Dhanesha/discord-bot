972dbfca313e0700861bbd5775bd082ea4f5f3a5
Dishit-Dhanesha <dishitrvdhanesha@gmail.com>
2021-10-15 19:50:37 +0530

    Package Error and Simple Interaction

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const Discord = require("discord.js")
-const { Client, Intents } = require('discord.js');
-require('dotenv').config()
+import { Client, Intents } from 'discord.js';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import Database from '@replit/database';
 
+dotenv.config()
+const db = new Database();
+
+
+const sadDeclarations = ["sad" , "depressed" , "unhappy" , "angry"]
+
+const encouragements = ["Cheer up!" , "Hang in there." , "You are a great person / bot!"]
+
+
+function getQuote(){
+    return fetch("https://zenquotes.io/api/random").then(res =>{
+        return res.json();
+    })
+    .then(data =>{
+        return data[0]["q"] + "-" + data[0]["a"]
+    })
+}
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -10,8 +28,15 @@ client.on("ready", ()=>{
 })
 
 client.on("message", msg =>{
-    if(msg.content === 'ping'){
-        msg.reply("pong")
+    if(msg.author.bot) return
+    if(msg.content === "$inspire"){
+        getQuote().then(quote => msg.channel.send(quote))
+    }
+    if(sadDeclarations.some(word => msg.content.includes(word))){
+        db.get("encouragements").then(encouragement =>{
+            const encouragment = encouragements[Math.floor(Math.random() * encouragements.length)]
+            msg.reply(encouragment);
+        })
     }
 })
 
